64a303530cb2554ecca9db4761adf9f8335bbff7
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-08-29 21:34:11 +0800

    edit the position of the cards

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import '../App.css'
 
@@ -8,7 +8,7 @@ function Certificates() {
 
   return (
     <section className="w-4/5 h-screen mt-14 left-56 p-7 absolute hide-scrollbar">
-    
+
       <div className="bg-White">
         <div className="flex justify-between items-center h-16 bg-White px-5 w-full border border-b-gray-300">
           <p className='text-xl'>Residence Certificate Issuance</p>
@@ -29,7 +29,7 @@ function Certificates() {
                 Barangay Clearance
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                This is a general clearance that verifies the person has no pending cases or derogatory records in the barangay. 
+                This is a general clearance that verifies the person has no pending cases or derogatory records in the barangay.
               </p>
             </div>
 
@@ -49,7 +49,7 @@ function Certificates() {
                 Certificate of Residency
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                This certifies that a person is a resident of the barangay. 
+                This certifies that a person is a resident of the barangay.
               </p>
             </div>
 
@@ -66,7 +66,7 @@ function Certificates() {
           <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
             <div class="p-5">
               <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-Green antialiased">
-               Certificate of Indigency
+                Certificate of Indigency
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
                 This certifies that the person or family belongs to the indigent sector of the community.
@@ -82,19 +82,19 @@ function Certificates() {
             </div>
           </div>
 
-          {/* This is for Certificate of Good Moral Character */}
+          {/* This is for Certificate of Low Income */}
           <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
             <div class="p-5">
               <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-Green antialiased">
-                Certificate of Good Moral Character
+                Certificate of Low Income
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                This certifies that the person is of good moral character and has no derogatory records in the barangay.
+                This certifies that the person or family has a low income.
               </p>
             </div>
 
             <div class="p-5 pt-0">
-              <NavLink to={'/certificate-of-good-moral-character'}>
+              <NavLink to={'/certificate-of-low-income'}>
                 <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-Green py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                   Issue
                 </button>
@@ -109,7 +109,7 @@ function Certificates() {
                 Business Clearance
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-              This is issued to business owners within the barangay and is a requirement for obtaining a business permit from the municipal or city hall.
+                This is issued to business owners within the barangay and is a requirement for obtaining a business permit from the municipal or city hall.
               </p>
             </div>
 
@@ -162,19 +162,19 @@ function Certificates() {
             </div>
           </div>
 
-          {/* This is for Certificate of Low Income */}
+          {/* This is for Certificate of Good Moral Character */}
           <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
             <div class="p-5">
               <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-Green antialiased">
-                Certificate of Low Income
+                Certificate of Good Moral Character
               </h5>
               <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                This certifies that the person or family has a low income.
+                This certifies that the person is of good moral character and has no derogatory records in the barangay.
               </p>
             </div>
 
             <div class="p-5 pt-0">
-              <NavLink to={'/certificate-of-low-income'}>
+              <NavLink to={'/certificate-of-good-moral-character'}>
                 <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-Green py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                   Issue
                 </button>
@@ -224,8 +224,8 @@ function Certificates() {
 
 
         </div>
-        
-        
+
+
       </div>
 
 
