930b3564c0ab2cff84651469e89631f943d3b11c
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-09-02 15:22:02 +0800

    add announcement page in admin side

--- src/pages/Announcement.jsx
+++ src/pages/Announcement.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import axios from 'axios';
-import { FaCheck, FaSpinner, FaEdit, FaTrash } from 'react-icons/fa'; // Import additional icons
+import { FaCheck, FaSpinner, FaEdit, FaTrash } from 'react-icons/fa';
 
 // AWS S3 setup
 const bucketName = import.meta.env.VITE_AWS_BUCKET_NAME;
